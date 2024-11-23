028e346a74201d9cf5dcf34bb792b2bd89884bab
Alec Molloy <alecmolloy@users.noreply.github.com>
2024-11-22 20:19:32 -0500

    react 19

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,24 +1,30 @@
 import NextLink from 'next/link'
 import React from 'react'
 
-type LinkProps = React.ComponentProps<typeof NextLink>
+type LinkProps = React.ComponentProps<typeof NextLink> & {
+  ref?: React.RefObject<HTMLAnchorElement>
+}
 
-export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  function Link({ href, children, style, ...props }, ref) {
-    return (
-      <NextLink
-        {...props}
-        style={{
-          color: '#0070f3',
-          textDecorationStyle: 'wavy',
-          textDecorationThickness: '1.5px',
-          ...style,
-        }}
-        ref={ref}
-        href={href}
-      >
-        {children}
-      </NextLink>
-    )
-  },
-)
+export const Link: React.FC<LinkProps> = ({
+  href,
+  children,
+  style,
+  ref,
+  ...props
+}) => {
+  return (
+    <NextLink
+      {...props}
+      style={{
+        color: '#0070f3',
+        textDecorationStyle: 'wavy',
+        textDecorationThickness: '1.5px',
+        ...style,
+      }}
+      ref={ref}
+      href={href}
+    >
+      {children}
+    </NextLink>
+  )
+}
